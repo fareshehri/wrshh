ad94107a5365b066d28a0688ff6b9d301339466e
Faris Alshehri <fareshehri@gmail.com>
2022-12-24 13:04:06 +0300

    12/24/2022

--- lib/Pages/Editaccount.dart
+++ lib/Pages/Editaccount.dart
@@ -25,30 +25,36 @@ class _EditaccountState extends State<Editaccount> {
     return Scaffold(
     appBar: AppBar(title: const Text('Edit Account'),centerTitle: true,backgroundColor: Colors.transparent,foregroundColor: Colors.black,elevation: 0,iconTheme:IconThemeData(color: Colors.lightBlue[300],size: 24),),
 
-    body: Center(child: Form(key: _formKey,child: Column(children: <Widget>[
-                  Text('Name'),
-                  TextFormField(decoration: const InputDecoration(contentPadding: EdgeInsets.zero),inputFormatters: [FilteringTextInputFormatter.digitsOnly,LengthLimitingTextInputFormatter(17)],validator: (value) {if (value == null || value.isEmpty ) {return 'Please enter VIN Number';}return null;},),
-                  Text('Email'),
-                  TextFormField(),
-                  Text('Phone'),
-                  TextFormField(),
-                  Text('City'),
-                  TextFormField(),
-                  Text('Car'),
-                  TextFormField(),
-                  
-                  ElevatedButton.icon(onPressed: () {
-                    // Validate returns true if the form is valid, or false otherwise.
-                    if (_formKey.currentState!.validate()) {
-                      // If the form is valid, display a snackbar. In the real world,
-                      // you'd often call a server or save the information in a database.
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),);}},
-                        icon: const Icon(Icons.search_rounded,), label: const Text('Search'),)
-        
-                ],
-      ),
-      )),
+    body: Center(child: Container(padding:EdgeInsets.all(50) ,
+      child: Form(key: _formKey,child: Column(children: <Widget>[
+                    Text('Name'),
+                    TextFormField(decoration: const InputDecoration(contentPadding: EdgeInsets.zero),inputFormatters: [FilteringTextInputFormatter.digitsOnly,LengthLimitingTextInputFormatter(17)],validator: (value) {if (value == null || value.isEmpty ) {return 'Please enter VIN Number';}return null;},),
+                    SizedBox(height: 20,),
+                    Text('Email'),
+                    TextFormField(),
+                    SizedBox(height: 20,),
+                    Text('Phone'),
+                    TextFormField(),
+                    SizedBox(height: 20,),
+                    Text('City'),
+                    TextFormField(),
+                    SizedBox(height: 20,),
+                    Text('Car'),
+                    TextFormField(),
+                    SizedBox(height: 20,),
+                    ElevatedButton.icon(onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+                        // If the form is valid, display a snackbar. In the real world,
+                        // you'd often call a server or save the information in a database.
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),);}},
+                          icon: const Icon(Icons.save_as,), label: const Text('Edit'),)
+          
+                  ],
+        ),
+        ),
+    )),
 
     );
     
