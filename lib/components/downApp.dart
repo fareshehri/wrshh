622fc5f6e38f86999b2978b9d1dfef948f37cf4b
Faris Alshehri <fareshehri@gmail.com>
2023-02-03 19:53:05 +0300

    Update

--- lib/components/downApp.dart
+++ lib/components/downApp.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:open_document/open_document.dart';
 Future<void> downloadApp(Uint8List byteslist) async{
-  final output = await getTemporaryDirectory();
+  final output = await getApplicationDocumentsDirectory();
     var filePath = "${output.path}/report.pdf";
     final file = File(filePath);
     await file.writeAsBytes(byteslist);
